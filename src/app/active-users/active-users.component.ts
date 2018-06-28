7536a035d45783ba1ee31c0cf60a0e8ea3bf7c52
Balaskandan <balaskandangiri@gmail.com>
2018-06-28 12:21:03 +0530

    active users 04

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -11,20 +11,24 @@ export class ActiveUsersComponent implements OnInit {
  
   private userData = null;
   private userPics = [];
-
+  images =["char_male_basic.png","char_male_emo.png","char_male_programmer.png"];
+  
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
+
     this.http.get('http://gymificationcodeathon-env.8vvmhjujd2.ap-south-1.elasticbeanstalk.com/users/active').subscribe(data => {
       this.userData = data;
-    for(let user of this.userData) {
-      this.http.get             ('http://gymificationcodeathon-env.8vvmhjujd2.ap-south-1.elasticbeanstalk.com/image?userName=' + user.userName).subscribe(data => {
-      this.userPics.push(data);
-      });
-    }
-
-    console.log("Hello"+this.userData);
+      console.log(this.images[Math.floor(Math.random()*3)]);
+    // for(let user of this.userData) {
+    //   console.log("test");
+    //   this.http.get             ('http://gymificationcodeathon-env.8vvmhjujd2.ap-south-1.elasticbeanstalk.com/image?userName=' + user.userName).subscribe(data => {
+    //   this.userPics.push(data);
+    //   });
+    //   console.log("userPics->"+this.userPics);
+    // }
+    
   });
 }
 
